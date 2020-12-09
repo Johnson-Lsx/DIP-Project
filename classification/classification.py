50d30abc14f6e90b095477fe7df35a6eae1932de
Lin Shaoxiong <2410015101@qq.com>
2020-12-09 15:02:00 +0800

    修改了错误的停止训练的判断逻辑

--- classification/classification.py
+++ classification/classification.py
@@ -195,8 +195,10 @@ def train_model(dataloaders, image_datasets, model, criterion, optimizer, schedu
                 cnt = 0
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
-            else:
+            elif phase == 'val' and epoch_acc < best_acc:
                 cnt += 1      
+            else:
+                continue
         plot_curve(train_acc, dev_acc, 'acc', prefix)
         plot_curve(train_ls, dev_ls, 'ls', prefix)
         if cnt == 10:
